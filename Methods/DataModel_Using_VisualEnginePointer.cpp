e4e3b5323f4d816c43b5de2eaba4d307da5f7744
Twisted <winkleytyler64@gmail.com>
2025-12-08 16:26:30 -0500

    Delete Methods/DataModel_Using_VisualEnginePointer.cpp

--- Methods/DataModel_Using_VisualEnginePointer.cpp
+++
@@ -1,4 +0,0 @@
-uintptr_t VisualEngine = read<uintptr_t>(mem::BaseAddress + offsets::VisualEnginePointer);
-uintptr_t DataModelPointer = read<uintptr_t>(VisualEngine + offsets::VisualEngineToDataModel1);
-uintptr_t DataModel = read<uintptr_t>(DataModelPointer + offsets::VisualEngineToDataModel2);
-// Wow this is so UD!
